b6d8aac8b1afe2c72d808d5880411920f6f8e5c7
Michael Mogenson <michael.mogenson@gmail.com>
2020-12-26 13:52:21 -0700

    interrupt driven uart echo

--- uart-echo/src/main.rs
+++ uart-echo/src/main.rs
@@ -1,19 +1,41 @@
 #![no_std]
 #![feature(start)]
 
-use embedded_hal::prelude::*;
-use gba::io::sio::{BaudRate, SioSerial};
-use nb::block;
+use gba::io::{
+    irq::{set_irq_handler, IrqEnableSetting, IrqFlags, BIOS_IF, IE, IF, IME},
+    sio::{BaudRate, IoControlSetting, SioControlSetting, SioMode, RCNT, SIOCNT, SIODATA8},
+};
 use panic_abort as _;
 
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    let mut serial = SioSerial;
-    SioSerial::init(BaudRate::Bps115200);
+    RCNT.write(IoControlSetting::new());
+    SIOCNT.write(
+        SioControlSetting::new()
+            .with_baud_rate(BaudRate::Bps115200)
+            .with_data_length_8bit(true)
+            .with_mode(SioMode::Uart)
+            .with_rx_enable(true)
+            .with_tx_enable(true)
+            .with_irq_enable(true),
+    );
 
-    loop {
-        if let Ok(c) = block!(serial.read()) {
-            block!(serial.write(c)).ok();
+    set_irq_handler(irq_handler);
+    IE.write(IrqFlags::new().with_serial(true));
+    IME.write(IrqEnableSetting::IRQ_YES);
+
+    loop {}
+}
+
+extern "C" fn irq_handler(flags: IrqFlags) {
+    // clear all interrupts by writing the set bits back
+    BIOS_IF.write(BIOS_IF.read());
+    IF.write(IF.read());
+    if flags.serial() {
+        if !SIOCNT.read().rx_empty() {
+            let mut data = SIODATA8.read() as u8 as char;
+            data.make_ascii_uppercase();
+            SIODATA8.write(data as u16);
         }
     }
 }
